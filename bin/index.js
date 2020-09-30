798192d44e0c877498a5faf2ffbf0ad01d26475e
Ralph' Potato <fpilot.github@gmail.com>
2020-09-30 02:02:39 +0200

    Add CLI

---
+++ bin/index.js
@@ -0,0 +1,155 @@
+#!/usr/bin/env node
+const path = require('path')
+const fs = require('fs')
+const babel = require('@babel/core')
+const tsNode = require('ts-node')
+
+const [ _1, _2, method, htmlPath, sheetPath, workersPath, distPath, port ] = process.argv
+
+/*
+**  Helping
+*/
+
+class HtmlPage {
+  constructor (html, styleSheet = null) {
+    this.html = html
+    this.styleSheet = styleSheet
+  }
+
+  replaceTag (tag, content) {
+    const regex = new RegExp(`\{\{${tag}\}\}`)
+    return new HtmlPage(this.html.replace(regex, content), this.styleSheet)
+  }
+
+  optionalReplaceTag (tag, content) {
+    if (content === null) {
+      return this
+    }
+    return this.replaceTag(tag, content)
+  }
+}
+
+// GET Html Page and build CSS aside
+const getHtmlPage = basis => {
+  const { default: _content } = require(path.resolve(sheetPath))
+
+  if (typeof _content !== 'string') {
+    throw new Error(`Could not load HTML from sheet location "${sheetPath}"`)
+  }
+
+  const styleTagsRegex = /<style data-emotion-css="(?:.*?)>(?<classes>.*?)<\/style>/g
+
+  const classes = []
+  let matches = []
+  while (matches = styleTagsRegex.exec(_content)) {
+    classes.push(matches[1].replace(/\.css-/g, '.sheet-css-'))
+  }
+
+  // Build CSS
+  const styleSheet = `${sheet}.css`
+  fs.writeFileSync(path.resolve(distPath, styleSheet), classes.join('\n'))
+  console.log(`> "${styleSheet}" built !`)
+  const content = (
+    _content
+      .replace(styleTagsRegex, '')
+      .replace(/class="css-(.*?)"/g, 'class="sheet-css-$1"')
+  )
+
+  // Embed JS
+  const js = (
+    babel.transformFileSync(workersPath).code
+  ) || null
+
+  // Return bundled HTML
+  return (
+    new HtmlPage(basis, styleSheet)
+      .replaceTag('CONTENT', content)
+      .replaceTag('SHEET_NAME', sheet)
+      .optionalReplaceTag('SCRIPTS', js)
+  )
+}
+
+/*
+**  Scripting
+*/
+
+const usageGuide =
+`\nUsage :
+  - ts-roll20 --build [htmlPath] [sheetPath] [workersPath] [distPath]
+  - ts-roll20 --preview [htmlPath] [sheetPath] [workersPath] [distPath] [port]
+`
+
+const checkStringArg = key => arg => () => {
+  if (typeof arg !== 'string') {
+    throw new Error(`Argument [${key}] was not provided`)
+  }
+}
+
+const checkNumberArg = key => arg => () => {
+  if (isNaN(parseInt(arg, 10))) {
+    throw new Error(`Argument [${key}] was not or not a number`)
+  }
+}
+
+const checkHtmlPath = checkStringArg('htmlPath')(htmlPath)
+const checkSheetPath = checkStringArg('sheetPath')(sheetPath)
+const checkWorkersPath = checkStringArg('workersPath')(workersPath)
+const checkDistPath = checkStringArg('distPath')(distPath)
+const checkPort = checkNumberArg('port')(port)
+
+const registerTsNode = () => {
+  tsNode.register({
+    typeCheck: true
+  })
+}
+
+switch (method) {
+  // Build HTML, CSS and JS
+  case '--build': {
+    checkHtmlPath()
+    checkSheetPath()
+    checkWorkersPath()
+    checkDistPath()
+    registerTsNode()
+
+    const basis = fs.readFileSync(path.resolve(htmlPath), 'utf-8')
+    const MainPage = getHtmlPage(basis, sheet)
+
+    fs.writeFileSync(path.resolve(baseDir, `${sheet}.html`), MainPage.html)
+    break
+  }
+
+  // Start preview dev server
+  case '--preview': {
+    checkHtmlPath()
+    checkSheetPath()
+    checkWorkersPath()
+    checkDistPath()
+    checkPort()
+    registerTsNode()
+
+    const basis = fs.readFileSync(path.resolve(htmlPath), 'utf-8')
+    const MainPage = getHtmlPage(basis, sheet)
+
+    http.createServer((req, res) => {
+      if (req.url === '/style') {
+        res.writeHead(200, { 'Content-Type': 'text/css' })
+        if (MainPage.styleSheet !== null) {
+          res.write(fs.readFileSync(path.resolve(distPath, MainPage.styleSheet), 'utf-8'))
+        }
+        res.end()
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(MainPage.html)
+        res.end()
+      }
+    }).listen(port)
+    break
+  }
+
+  // Unknown command
+  default: {
+    console.log(usageGuide)
+    break
+  }
+}
